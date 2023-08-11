e19d2722384725306111978a5fb0674588c2740e
Lore <lore@localhost.localdomain>
2023-08-10 18:28:41 -0700

    Parse input string and give confirmation output

--- dice-roller.el
+++ dice-roller.el
@@ -1,6 +1,9 @@
 (setq dice-regex "\\([0-9]*\\)[dD]\\([0-9]+\\)")
 
-;; I don't know how to do this yet.
-
-(defun roll-dice (instr)
-  nil)
+(defun roll-dice (input)
+  (interactive "Mdice: ")
+  (save-match-data (if (string-match dice-regex input)
+                       (progn (message "You want to roll %s %s-sided dice."
+                                       (match-string 1 input)
+                                       (match-string 2 input)))
+                     (message "That is not a valid dice string."))))
